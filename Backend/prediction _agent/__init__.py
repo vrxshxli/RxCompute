28fe01fabee7fa2a3badfd4af3474e3263c8197f
Deepak Mishra <121deepak2104@sjcem.edu.in>
2026-02-28 19:03:14 +0530

    removed duplicate folders

--- Backend/prediction _agent/__init__.py
+++
@@ -1,7 +0,0 @@
-from prediction_agent.prediction_agent import (
-    run_prediction_scan,
-    run_prediction_for_user,
-    run_demand_forecast,
-)
-
-__all__ = ["run_prediction_scan", "run_prediction_for_user", "run_demand_forecast"]
