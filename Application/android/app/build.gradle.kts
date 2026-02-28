249243691122391282f42dbea8eb37f7a0ef7467
Deepak Mishra <121deepak2104@sjcem.edu.in>
2026-02-28 08:47:00 +0530

    Updated push notiification =

--- Application/android/app/build.gradle.kts
+++ Application/android/app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -72,4 +73,5 @@ flutter {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
